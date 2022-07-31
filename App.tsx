e79d46bfb5fbc7533fb9d5bcdc7ec66718a699f4
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-07-30 21:19:37 -0300

    Criando estilos globais

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from "styled-components"
+import { StatusBar } from 'expo-status-bar';
+
 import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
       <Dashboard />
-    </>
+    </ThemeProvider>
   );
 }
