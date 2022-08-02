8b5cfbf289e6e1b1b8c208bc28ff248aa64e55a2
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-08-02 20:43:58 -0300

    Utilizando proporções em medidas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { ThemeProvider } from "styled-components"
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from "styled-components";
 import { StatusBar } from 'expo-status-bar';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Container, Title } from './styles'
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles'
 
 export function Dashboard(){
   return(
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/22225821?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Gabriel</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
