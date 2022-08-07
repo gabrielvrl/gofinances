6895f030214fee9ed1923ee4b977a10a1984111a
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-08-07 14:37:57 -0300

    Tipando e acessando propriedadesno Styled Components

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,18 +10,35 @@ import {
   LastTransaction,
 } from './styles';
 
-export function HighlightCard(){
+interface Props {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle',
+  total: 'dollar-sign',
+}
+
+export function HighlightCard({ title, amount, lastTransaction, type } : Props){
   return(
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entrada</Title>
-        <Icon name="arrow-up-circle" />
+        <Title type={type}>
+          {title}
+        </Title>
+        <Icon name={icon[type]} type={type} />
       </Header>
       
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>
-          Última entrada 13 de abril
+        <Amount type={type}>
+          {amount}
+        </Amount>
+        <LastTransaction type={type}>
+          {lastTransaction}
         </LastTransaction>
       </Footer>
     </Container>
