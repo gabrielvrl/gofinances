a3a39dc2f8ff39b215b4720a604c8e4d10cd1ecf
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-08-10 22:27:11 -0300

    Utilizando Listas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -11,12 +11,44 @@ import {
   Icon,
   HighlightCards,
   Transactions,
-  Title
+  Title,
+  TransactionList
 } from './styles'
 import { HighlightCard } from "../../components/HighlightCard";
 import { TransactionCard } from "../../components/TransactionCard";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 export function Dashboard(){
+  const data = [
+    {
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020",
+    },
+    {
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020",
+    },
+    {
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020",
+    },
+  ];
+
   return(
     <Container>
       <Header>
@@ -57,7 +89,15 @@ export function Dashboard(){
       <Transactions>
         <Title>Listagem</Title>
 
-        <TransactionCard />
+        <TransactionList
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: getBottomSpace()
+          }}
+        />
+
       </Transactions>
     </Container>
   )
