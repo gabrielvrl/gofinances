2a808fa6b2380422608440f6733dd4e29ca92e75
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-02-08 07:29:29 -0300

    Utilizando Mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -3,8 +3,31 @@ import { renderHook, act } from '@testing-library/react-hooks';
 
 import { AuthProvider, useAuth } from './auth';
 
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => {
+      return {
+        type: 'success',
+        params: {
+          access_token: 'google-token'
+        }
+      }
+    }   
+  }
+});
+
 describe('Auth Hook', () => {
   it('should be able to Sign In with Google Account', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve({
+        id: `userInfo.id`,
+        email: `userInfo.email`,
+        name: `userInfo.given_name`,
+        photo: `userInfo.picture`,
+        locale: `userInfo.locale`,
+        verified_email: `userInfo.verified_email`,
+      })
+    })) as jest.Mock;
     const { result } = renderHook(() => useAuth(),{ wrapper: AuthProvider });
 
     await act(() => result.current.signInWithGoogle())
